b924df09584b15e1b0ecbdf30e326a2ee9aecf08
Camrie Madsen <camrie.madsen@aexp.com>
2018-07-27 15:49:40 -0700

    update background.js

--- background.js
+++ background.js
@@ -10,3 +10,9 @@ chrome.runtime.onInstalled.addListener(function(details) {
       });
 });
 
+chrome.tabs.onUpdated.addListener(function(id, info, tab){
+    if (tab.url.toLowerCase().indexOf("facebook.com") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+});
+
